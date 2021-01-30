78243889bcef6ed3c3e94bb19a03f35728acede2
Lorenz Sieben <me@lorenz-sieben.com>
2021-01-30 14:38:46 +0100

    Add test map

---
+++ BaseChip_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../rc3_tiles/imgs/tilesheets/BaseChip_pipo.png" width="256" height="4256"/>
+</tileset>
